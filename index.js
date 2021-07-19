bc81ba35d1d3bb0e8283b58d39cf638bd1b143a3
conman228 <rinaldocityboy@gmail.com>
2021-07-19 09:31:37 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,38 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
+const fs = require('fs');
 const path = require('path');
-
 const dotenv = require('dotenv');
-// Import required bot configuration.
-const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
-
 const restify = require('restify');
-
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const readline = require('readline');
+const { EchoBot } = require('./bot');
+const ENV_FILE = path.join(__dirname, '.env');
 const { BotFrameworkAdapter } = require('botbuilder');
 
-// This bot's main dialog.
-const { EchoBot } = require('./bot');
+dotenv.config({ path: ENV_FILE });
+
+const conversationReferences = {};
+const myBot = new EchoBot(conversationReferences);
 
 // Create HTTP server
 const server = restify.createServer();
+const adapter = new BotFrameworkAdapter({});
 server.listen(process.env.port || process.env.PORT || 3978, () => {
   
 });
 
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
-const adapter = new BotFrameworkAdapter({
-});
-
 // Catch-all for errors.
 const onTurnErrorHandler = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
-
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
         `${ error }`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
-
-    // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
-// Set the onTurnError for the singleton BotFrameworkAdapter.
 adapter.onTurnError = onTurnErrorHandler;
-
-// Create the main dialog.
-const myBot = new EchoBot();
-
-// Listen for incoming requests.
 
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
@@ -62,9 +40,36 @@ server.post('/api/messages', (req, res) => {
         await myBot.run(context);
     });
 });
+
 server.use(restify.plugins.bodyParser());
-server.post('/api/data', (req, res) => {
-    //const data = JSON.parse(req);
-    console.log(req.body);
-    res.send(200);
+
+server.post('/api/data', async (req, res) => {
+    for (const conversationReference of Object.values(conversationReferences)) {
+        await adapter.continueConversation(conversationReference, async turnContext => {
+            await turnContext.sendActivity(req.body);
+
+            let bodyText = req.body;
+            let jobname = "";
+            let joburl = "";
+
+            jobname = bodyText.substring(bodyText.indexOf("JOB NAME:")+10, bodyText.indexOf("JOB URL:"));
+            jobname = jobname.replace(/\n/g, '');
+            jobname = jobname.substring(3, jobname.length);
+
+            joburl = bodyText.substring(bodyText.indexOf("JOB URL:")+9, bodyText.length);
+            joburl = joburl.replace(/\n/g, '');
+
+            linkFile("JOB NAME: " + jobname + "\n\n   JOB URL: "+ joburl);
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    res.end();
 });
+  
+  
+function linkFile(linkText){
+    fs.appendFile('link.txt', "\n - " + linkText + "\n", (err)=>{
+        if(err) throw(err);
+    });
+}
